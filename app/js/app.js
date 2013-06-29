367f0e295146e24c8614a4c6bac9cb0e9dc7e3c6
Bjarke Hesthaven Søndergaard <tixusdk@gmail.com>
2013-06-29 08:33:07 +0200

    Added a scrollfix

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,4 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('bjarkehs', ['bjarkehs.filters', 'bjarkehs.services', 'bjarkehs.directives', 'bjarkehs.controllers', 'ngSanitize']);
+angular.module('bjarkehs', ['bjarkehs.filters', 'bjarkehs.services', 'bjarkehs.directives', 'bjarkehs.controllers', 'ngSanitize', 'ui.scrollfix']);
--- app/js/directives.js
+++ app/js/directives.js
@@ -9,3 +9,24 @@ app.directive('appVersion', ['version', function(version) {
         elm.text(version);
     };
 }]);
+
+app.directive('sticky', ['$window', function($window) {
+    return function(scope, element, attrs) {
+        var windowElem = angular.element($window);
+        var elementOffset = $(element).offset().top-40;
+        windowElem.bind('scroll', function(e) {
+            if (elementOffset-$(windowElem).scrollTop() < 0) {
+                attrs.$set('class', 'fixed');
+            } else {
+                attrs.$set('class', '');
+            }
+         });
+        // scope.offset = scope[attrs.scrollPosition];
+        // var windowEl = angular.element($window);
+        //     windowEl.on('scroll', function() {
+        //         scope.$apply(function() {
+        //             scope[attrs.scrollPosition] = windowEl.scrollTop();
+        //         });
+        //     });
+    };
+}]);
